dcf83dcc2cb9d3417821993483419acc3edf20e2
Rejoice Okafor <112861600+Rejoice-Okafor@users.noreply.github.com>
2025-09-03 12:27:17 +0100

    Update assign-licenses.ps1

--- automation-user-provisioning/scripts/assign-licenses.ps1
+++ automation-user-provisioning/scripts/assign-licenses.ps1
@@ -1,1 +1,13 @@
+# 02-assign-licenses.ps1
+# Assign Microsoft 365 licenses to a user
+
+Connect-MgGraph -Scopes "User.ReadWrite.All"
+
+$UserId = "jdoe@yourtenant.onmicrosoft.com"
+$SkuId = "ENTERPRISEPACK" # Example: Office 365 E3
+
+# Assign license
+Set-MgUserLicense -UserId $UserId -AddLicenses @{SkuId=$SkuId}
+
+Write-Host "License assigned to $UserId successfully."
 
